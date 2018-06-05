f30c5f1b09b9fb60637a3a5755e0e7e0676fdf73
Uroš Vaupotič <ugi.vaupotic@gmail.com>
2018-06-05 11:43:39 +0200

    Json parsed into ArrayList

--- app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/MainActivity.java
@@ -13,7 +13,10 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     Button osvezi;
-    public static TextView opis;
+    public static TextView closest_name;
+    public static ImageView closest_image;
+    public static TextView closest_distance;
+    public static TextView closest_num_slots;
     fetchData process = new fetchData();
 
     @Override
@@ -22,7 +25,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         osvezi = (Button) findViewById(R.id.osvezi_button);
-        opis = (TextView) findViewById(R.id.name_closest_text_view);
+        closest_name = (TextView) findViewById(R.id.name_closest_text_view);
+        closest_image = (ImageView) findViewById(R.id.closest_parking_image_view);
+        closest_distance = (TextView) findViewById(R.id.distance_closest_text_view);
+        closest_num_slots = (TextView) findViewById(R.id.nr_of_places_closest_text_view);
 
 
         process.execute();
--- app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/fetchData.java
+++ app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/fetchData.java
@@ -1,17 +1,28 @@
 package si.uni_lj.fe.uv0414.parkaway_ljubljana;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class fetchData extends AsyncTask<Void, Void, Void> {
     String data;
+    ArrayList<String[]> dataList = new ArrayList<String[]>();
+    private static final String TAG = "SPOROCILO";
+    JSONArray JA;
     @Override
     protected Void doInBackground(Void... voids) {
         try {
@@ -24,11 +35,28 @@ public class fetchData extends AsyncTask<Void, Void, Void> {
             while (line != null){
                 line = bufferedReader.readLine();
                 data += line;
-                //Tukaj bomo se parsali
+            }
+            data = data.substring(4);
+            JA = new JSONArray(data);
+
+            for (int i = 0; i < JA.length(); i++){
+                String [] dataParsed = new String[7];
+                JSONObject JO = (JSONObject) JA.get(i);
+                dataParsed[0] = (String) JO.get("name");
+                dataParsed[1] = (String) JO.get("description");
+                dataParsed[2] = (String) JO.get("image_path");
+                dataParsed[3] = (String) JO.get("num_slots");
+                dataParsed[4] = (String) JO.get("price");
+                dataParsed[5] = (String) JO.get("lat");
+                dataParsed[6] = (String) JO.get("lng");
+
+                dataList.add(dataParsed);
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
         return null;
@@ -37,6 +65,10 @@ public class fetchData extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-        MainActivity.opis.setText(this.data);
+        Log.i(TAG, dataList.toString());
+        MainActivity.closest_name.setText(this.dataList.get(1)[0]);
+       // MainActivity.closest_image.setImageURI(this.dataList.get(0)[0]);
+       // MainActivity.closest_distance.setText(this.dataList.get(0)[0]);
+        MainActivity.closest_num_slots.setText(this.dataList.get(1)[3] + " mest");
     }
 }
