ee2577560f5c98dca513ed19eee7fcac9fbd3db8
Uroš Vaupotič <ugi.vaupotic@gmail.com>
2018-06-05 09:20:30 +0200

    progress

--- app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/MainActivity.java
+++ app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/MainActivity.java
@@ -1,13 +1,57 @@
 package si.uni_lj.fe.uv0414.parkaway_ljubljana;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button osvezi;
+    public static TextView opis;
+    fetchData process = new fetchData();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        osvezi = (Button) findViewById(R.id.osvezi_button);
+        opis = (TextView) findViewById(R.id.name_closest_text_view);
+
+
+        process.execute();
+
+        osvezi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                process.execute();
+            }
+        });
+
+        ImageView closest = (ImageView) findViewById(R.id.closest_parking_image_view);
+
+        closest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent parkingInfoIntent = new Intent(MainActivity.this, ParkingActivity.class);
+                startActivity(parkingInfoIntent);
+            }
+        });
+
+        Button map = (Button) findViewById(R.id.zemljevid_button);
+        map.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent showMapIntent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(showMapIntent);
+            }
+        });
     }
 }
