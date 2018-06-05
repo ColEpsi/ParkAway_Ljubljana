de40989dd7a60188a6ad5822d2de869c7cea80fb
Luka Zakšek <luka.zaksek@gmail.com>
2018-06-05 09:27:24 +0200

    que

--- app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/ParkingActivity.java
+++ app/src/main/java/si/uni_lj/fe/uv0414/parkaway_ljubljana/ParkingActivity.java
@@ -2,6 +2,7 @@ package si.uni_lj.fe.uv0414.parkaway_ljubljana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class ParkingActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ParkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
+
+        // custom title bar
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar_layout);
     }
 }
